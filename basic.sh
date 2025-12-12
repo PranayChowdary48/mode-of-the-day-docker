7bbd201b80af263193b43801b37a0f6988908f5a
Pinapaka, Pranaychowdary | Pranay | TTSD <pranaychowd.pinapaka@rakuten.com>
2025-12-12 16:01:04 +0900

    updated basic.sh with more commands

--- basic.sh
+++ basic.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Display docker version
 docker --version
+# run a special container to check docker
 docker run hello-world
-docker container run centos echo 'Hello World!'
+# pull image from docker hub
+docker pull alpine
+# pull and run a container
+docker container run centos:7 echo 'Hello World!'
+# run a container in background and keep it alive
+docker container run -di alpine
+# stop running containers
+docker stop $(docker ps -q) 
+# remove exited containers
+docker rm $(docker ps -a -q -f status=exited)
+# remove images
+docker rmi $(docker images -q)
